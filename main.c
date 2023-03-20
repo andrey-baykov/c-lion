f663118f20c618f5ba90af2b9e61da4dfcab196d
Baykov Andrey <baykov.andrey@gmail.com>
2023-03-19 23:08:11 -0700

    wip

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ void printGreeting() {
     printf("Good luck!\n");
 }
 
-void printMinMax(int a, int b){
-    if (a < b){
+void printMinMax(int a, int b) {
+    if (a < b) {
         printf("Min: %d\n", a);
         printf("Max: %d\n", b);
     } else {
@@ -15,8 +15,24 @@ void printMinMax(int a, int b){
         printf("Max: %d\n", a);
     }
 }
+
+float avgNumber() {
+    float num1, num2, num3, sum;
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    printf("Enter second number: ");
+    scanf("%f", &num2);
+    printf("Enter second number: ");
+    scanf("%f", &num3);
+    sum = num1 + num2 + num3;
+    return sum / 3.0;
+}
+
 int main() {
-    printGreeting();
-    printMinMax(23, 7);
+//    printGreeting();
+//    printMinMax(23, 7);
+    float max;
+    max = avgNumber();
+    printf("Max: %f", max);
     return 0;
 }
