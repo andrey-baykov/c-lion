95ec590a7813c9d06b7761ed1c7f5be6d5cf9479
Baykov Andrey <baykov.andrey@gmail.com>
2023-03-19 22:52:07 -0700

    wip

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main() {
+void printGreeting() {
+    printf("Hello!\n");
+    printf("Welcome to study C!\n");
+    printf("Good luck!\n");
+}
 
+void printMinMax(int a, int b){
+    if (a < b){
+        printf("Min: %d\n", a);
+        printf("Max: %d\n", b);
+    } else {
+        printf("Min: %d\n", b);
+        printf("Max: %d\n", a);
+    }
+}
+int main() {
+    printGreeting();
+    printMinMax(23, 7);
     return 0;
 }
