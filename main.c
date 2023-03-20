c60d53d0fc507ff1b488a4d79623933a401ea22c
Baykov Andrey <baykov.andrey@gmail.com>
2023-03-19 17:56:24 -0700

    wip

--- main.c
+++ main.c
@@ -8,13 +8,27 @@
 //    return 0;
 //}
 
-int main() {
-    char myChar1 = 'a';
-    char myChar2 = 97;
-    char myChar3 = 0x61;
-    char myChar4 = 0b01100001;
+//int main() {
+//    char myChar1 = 'a';
+//    char myChar2 = 97;
+//    char myChar3 = 0x61;
+//    char myChar4 = 0b01100001;
+//
+//    printf("Character: %c %c %c %c \n", myChar1, myChar2, myChar3, myChar4);
+//
+//    return 0;
+//}
 
-    printf("Character: %c %c %c %c \n", myChar1, myChar2, myChar3, myChar4);
-
+int main(){
+    int year, month, day;
+    printf("Enter year: ");
+    scanf("%d", &year);
+    printf("Enter month: ");
+    scanf("%d", &month);
+    printf("Enter day: ");
+    scanf("%d", &day);
+    printf("Year: 0x%X\n", year);
+    printf("Month: 0x%X\n", month);
+    printf("Day: 0x%X\n", day);
     return 0;
 }
