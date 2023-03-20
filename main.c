1f65ede86a01d6ad829eef974196d36932be9617
Baykov Andrey <baykov.andrey@gmail.com>
2023-03-19 18:19:22 -0700

    wip

--- main.c
+++ main.c
@@ -19,16 +19,31 @@
 //    return 0;
 //}
 
+//int main(){
+//    int year, month, day;
+//    printf("Enter year: ");
+//    scanf("%d", &year);
+//    printf("Enter month: ");
+//    scanf("%d", &month);
+//    printf("Enter day: ");
+//    scanf("%d", &day);
+//    printf("Year: 0x%X\n", year);
+//    printf("Month: 0x%X\n", month);
+//    printf("Day: 0x%X\n", day);
+//    return 0;
+//}
+
 int main(){
-    int year, month, day;
-    printf("Enter year: ");
-    scanf("%d", &year);
-    printf("Enter month: ");
-    scanf("%d", &month);
-    printf("Enter day: ");
-    scanf("%d", &day);
-    printf("Year: 0x%X\n", year);
-    printf("Month: 0x%X\n", month);
-    printf("Day: 0x%X\n", day);
+    char userChar;
+    printf("Enter character: ");
+    scanf("%c", &userChar);
+    if (97 <= userChar && userChar <= 122)
+        printf("Lowercase Letter\n");
+    else if (65 <= userChar && userChar <= 90)
+        printf("Upper Letter\n");
+    else if (48 <= userChar && userChar <= 57)
+        printf("Digit\n");
+    else
+        printf("Other\n");
     return 0;
 }
