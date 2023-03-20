6861ed69ef2dbcd802053727a6e9ce01246d69ae
Baykov Andrey <baykov.andrey@gmail.com>
2023-03-19 17:47:25 -0700

    wip

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+//int main() {
+//    char myChar1 = 'a';
+//    printf("Character: %c \n", myChar1);
+//    printf("Decimal ASCII Code: %d \n", myChar1);
+//    printf("Hexadecimal ASCII Code: %X \n", myChar1);
+//    return 0;
+//}
+
 int main() {
     char myChar1 = 'a';
-    printf("Character: %c \n", myChar1);
-    printf("Decimal ASCII Code: %d \n", myChar1);
-    printf("Hexadecimal ASCII Code: %X \n", myChar1);
+    char myChar2 = 97;
+    char myChar3 = 0x61;
+    char myChar4 = 0b01100001;
+
+    printf("Character: %c %c %c %c \n", myChar1, myChar2, myChar3, myChar4);
+
     return 0;
 }
